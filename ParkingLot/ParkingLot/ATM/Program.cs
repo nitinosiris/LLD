970401f0d168945e4b618aaff92f5779918a6531
Nitin <nitinwartkar58@gmail.com>
2025-11-23 01:03:28 +0530

    ATM

---
+++ ParkingLot/ParkingLot/ATM/Program.cs
@@ -0,0 +1,24 @@
+﻿using ATM;
+
+var user = new User("Nitin");
+var bankAccount = new BankAccount(2000);
+var card = new Card("1234", bankAccount, 123);
+var atm = new Atm();
+
+var atmRoom = new AtmRoom(atm, user);
+
+atmRoom.Atm.PrintCurrentStatus();
+
+atmRoom.Atm.GetAtmState().InsertCard(atm, card);
+atmRoom.Atm.PrintCurrentStatus();
+
+atmRoom.Atm.GetAtmState().AuthenticatePin(atm, card, 123);
+atmRoom.Atm.PrintCurrentStatus();
+
+// atmRoom.Atm.GetAtmState().SelectOperation(atm, card, TransactionType.CheckBalance);
+// atmRoom.Atm.PrintCurrentStatus();
+
+atmRoom.Atm.GetAtmState().SelectOperation(atm, card, TransactionType.WithDraw);
+atmRoom.Atm.PrintCurrentStatus();
+
+atmRoom.Atm.GetAtmState().CashWithdraw(atm, card, 3000);
